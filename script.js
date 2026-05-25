b97644dfbc3f665479271efc73061b38974ea155
Sahil Sheikh <153609223+sahil-sheikh01@users.noreply.github.com>
2026-05-25 21:27:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const gallery = document.querySelector(".gallery");
+const leftAngle = document.querySelector(".fa-angle-left");
+const rightAngle = document.querySelector(".fa-angle-right");
+
+gallery.addEventListener("wheel", function(event){
+    event.preventDefault();
+    gallery.scrollLeft += event.deltaY;
+});
+
+rightAngle.addEventListener("click", function(){
+    let maxScroll = gallery.scrollWidth - gallery.clientWidth;
+
+    if(gallery.scrollLeft >= maxScroll){
+        gallery.scrollLeft = 0;
+    }else{
+        let imageWidth = gallery.querySelector("div").offsetWidth;
+        gallery.scrollLeft += imageWidth + 20;
+    }
+});
+
+leftAngle.addEventListener("click", function(){
+    let maxScroll = gallery.scrollWidth - gallery.clientWidth;
+
+    if(gallery.scrollLeft <= 0){
+        gallery.scrollLeft = maxScroll;
+    }else{
+        let imageWidth = gallery.querySelector("div").offsetWidth;
+        gallery.scrollLeft -= imageWidth + 20;
+    }
+});
